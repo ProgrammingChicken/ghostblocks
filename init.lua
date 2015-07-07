b9db998ab9a721953b2fcd0d8006f6cf985e2598
PChicken <tardisbluedoctorwho1@gmail.com>
2015-07-07 15:52:19 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -82,6 +82,69 @@ drop = 'ghostblocks:stone_with_mese',
 diggable=true,
 groups = {cracky=3, stone=1},
 })
+minetest.register_node("ghostblocks:stone_with_copper", {
+walkable=false,
+tiles={"default_stone.png^default_mineral_copper.png"},
+drop = 'ghostblocks:stone_with_copper',
+diggable=true,
+groups = {cracky=3, stone=1},
+})
+minetest.register_node("ghostblocks:stone_with_gold", {
+walkable=false,
+tiles={"default_stone.png^default_mineral_gold.png"},
+drop = 'ghostblocks:stone_with_gold',
+diggable=true,
+groups = {cracky=3, stone=1},
+})
+minetest.register_node("ghostblocks:goldblock", {
+walkable=false,
+tiles={"default_gold_block.png"},
+drop = 'ghostblocks:goldblock',
+diggable=true,
+groups = {cracky=3, stone=1},
+})
+minetest.register_node("ghostblocks:coalblock", {
+walkable=false,
+tiles={"default_coal_block.png"},
+drop = 'ghostblocks:coalblock',
+diggable=true,
+groups = {cracky=3, stone=1},
+})
+minetest.register_node("ghostblocks:steelblock", {
+walkable=false,
+tiles={"default_steel_block.png"},
+drop = 'ghostblocks:steelblock',
+diggable=true,
+groups = {cracky=3, stone=1},
+})
+minetest.register_node("ghostblocks:diamondblock", {
+walkable=false,
+tiles={"default_diamond_block.png"},
+drop = 'ghostblocks:diamondblock',
+diggable=true,
+groups = {cracky=3, stone=1},
+})
+minetest.register_node("ghostblocks:mese", {
+walkable=false,
+tiles={"default_mese_block.png"},
+drop = 'ghostblocks:mese',
+diggable=true,
+groups = {cracky=3, stone=1},
+})
+minetest.register_node("ghostblocks:bronzeblock", {
+walkable=false,
+tiles={"default_bronze_block.png"},
+drop = 'ghostblocks:bronzeblock',
+diggable=true,
+groups = {cracky=3, stone=1},
+})
+minetest.register_node("ghostblocks:copperblock", {
+walkable=false,
+tiles={"default_copper_block.png"},
+drop = 'ghostblocks:copperblock',
+diggable=true,
+groups = {cracky=3, stone=1},
+})
 minetest.register_node("ghostblocks:mossycobble", {
 walkable=false,
 tiles={"default_mossycobble.png"},
@@ -93,6 +156,83 @@ minetest.register_node("ghostblocks:cobble", {
 walkable=false,
 tiles={"default_cobble.png"},
 drop = 'ghostblocks:cobble',
+diggable=true,
+groups = {cracky=3, stone=1},
+})
+minetest.register_node("ghostblocks:stonebrick", {
+walkable=false,
+tiles={"default_stone_brick.png"},
+drop = 'ghostblocks:stonebrick',
+diggable=true,
+groups = {cracky=3, stone=1},
+})
+minetest.register_node("ghostblocks:desert_stone", {
+walkable=false,
+tiles={"default_desert_stone.png"},
+drop = 'ghostblocks:desert_stone',
+diggable=true,
+groups = {cracky=3, stone=1},
+})
+minetest.register_node("ghostblocks:desert_stonebrick", {
+walkable=false,
+tiles={"default_desert_stone_brick.png"},
+drop = 'ghostblocks:desert_stonebrick',
+diggable=true,
+groups = {cracky=3, stone=1},
+})
+minetest.register_node("ghostblocks:desert_cobble", {
+walkable=false,
+tiles={"default_desert_cobble.png"},
+drop = 'ghostblocks:desert_cobble',
+diggable=true,
+groups = {cracky=3, stone=1},
+})
+minetest.register_node("ghostblocks:sandstone", {
+walkable=false,
+tiles={"default_sandstone.png"},
+drop = 'ghostblocks:sandstone',
+diggable=true,
+groups = {cracky=3, stone=1},
+})
+minetest.register_node("ghostblocks:sandstonebrick", {
+walkable=false,
+tiles={"default_sandstone_brick.png"},
+drop = 'ghostblocks:sandstonebrick',
+diggable=true,
+groups = {cracky=3, stone=1},
+})
+minetest.register_node("ghostblocks:obsidian", {
+walkable=false,
+tiles={"default_obsidian.png"},
+drop = 'ghostblocks:obsidian',
+diggable=true,
+groups = {cracky=3, stone=1},
+})
+minetest.register_node("ghostblocks:obsidianbrick", {
+walkable=false,
+tiles={"default_obsidian_brick.png"},
+drop = 'ghostblocks:obsidianbrick',
+diggable=true,
+groups = {cracky=3, stone=1},
+})
+minetest.register_node("ghostblocks:snowblock", {
+walkable=false,
+tiles={"default_snow_block.png"},
+drop = 'ghostblocks:snowblock',
+diggable=true,
+groups = {cracky=3, stone=1},
+})
+minetest.register_node("ghostblocks:gravel", {
+walkable=false,
+tiles={"default_gravel.png"},
+drop = 'ghostblocks:gravel',
+diggable=true,
+groups = {cracky=3, stone=1},
+})
+minetest.register_node("ghostblocks:desert_sand", {
+walkable=false,
+tiles={"default_desert_sand.png"},
+drop = 'ghostblocks:desert_sand',
 diggable=true,
 groups = {cracky=3, stone=1},
 })
