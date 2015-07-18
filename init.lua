3c871be9c55bf00fd9f3e9bce68d7ebcf3e97554
PChicken <tardisbluedoctorwho1@gmail.com>
2015-07-18 13:10:31 -0400

    shortened init.lua

--- init.lua
+++ init.lua
@@ -4,13 +4,16 @@ Code by ProgrammingChicken
 Uses textures from enabled texture pack.
 ghostblocks are blocks which you can walk through.
 you craft them using the ghost-converter.
-g-converter converts regular blocks into ghostblocks with fuel and "ghost-essence"
-g-essence is obtained by digging coal and stone.
-coal gives 0.1 g-essence
-stone gives 0.05 g-essence
-to convert one block you need 15 g-essence and 4 seconds of fuel.
-so you would need to dig 300 stone or 150 coal to make one.
-very expensive, but *realistic*.
+g-converter converts regular blocks into ghostblocks with "ghost-essence" as fuel.
+g-essence is made with coalblocks, dirt, and stone. one coalblock covered in dirt gives you a dirty smoot.
+To make compressed stone, fill the crafting area with stone.
+Surround that in compressed stone, and you get a smoot-stone. 
+It's pretty dark, and it has a nice texture, so you might want to use that as a building material too.
+To make compressed dirt, fill the crafting area with dirt.
+Surround smoot-stone in compressed dirt, and you get a natural block.
+Looks like crap, yet it's very useful.
+put it in by itself and you get 3 ghost-essence.
+to convert one block you need 15 g-essence.
 ~ghostblocks~
 
 +=ATTENTION=+
@@ -18,224 +21,22 @@ This only supports blocks in default_game and my mods.
 Feel free to fork this mod.
 just gimme creds yo
 ]]--
-
-minetest.register_node("ghostblocks:dirt", {
-walkable=false,
-tiles={"default_dirt.png"},
-drop = 'ghostblocks:dirt',
-diggable = true,
-groups = {cracky=3, stone=1},
+--shortcuts
+b="stone_with_"
+t="stone^default_mineral_"
+l="block"
+--reference
+block={"dirt", "dirt_with_grass", "clay", "sand", "gravel", "cobble", "mossycobble", "stone", b.."coal", b.."iron", b.."diamond", b.."mese", b.."copper", b.."gold", "coal"..l, "steel"..l, "diamond"..l, "mese", "copper"..l, "gold"..l, "bronze"..l, "stonebrick", "desert_cobble", "desert_sand", "desert_stonebrick", "desert_stone", "sandstone", "sandstonebrick", "obsidian", "obsidianbrick", "snowblock",}
+texture={"dirt", "dirt_with_grass", "clay", "sand", "gravel", "cobble", "mossycobble", "stone", t.."coal", t.."iron", t.."diamond", t.."mese", t.."copper", t.."gold", "coal_"..l, "steel_"..l, "diamond_"..l, "mese_"..l, "copper_"..l, "gold_"..l, "bronze_"..l, "stone_brick", "desert_cobble", "desert_sand", "desert_stone_brick", "desert_stone", "sandstone", "sandstone_brick", "obsidian", "obsidian_brick", "snow_block",}
+for i in table.getn(blocks) do
+  minetest.register_node("ghostblocks:"..block[i], {
+  walkable=false,
+  tiles={"default_"..texture[i]},
+  drop = "ghostblocks:"..block[i]..".png",
+  diggable = true,
+  groups = {cracky=3, stone=1},
 })
-minetest.register_node("ghostblocks:dirt_with_grass", {
-walkable=false,
-tiles={"default_grass.png"},
-drop = 'ghostblocks:dirt_with_grass',
-diggable = true,
-groups = {cracky=3, stone=1},
-})
-minetest.register_node("ghostblocks:clay", {
-walkable=false,
-tiles={"default_clay.png"},
-drop = 'ghostblocks:clay',
-diggable=true,
-groups = {cracky=3, stone=1},
-})
-minetest.register_node("ghostblocks:sand", {
-walkable=false,
-tiles={"default_sand.png"},
-drop = 'ghostblocks:sand',
-diggable=true,
-groups = {cracky=3, stone=1},
-})
-minetest.register_node("ghostblocks:stone", {
-walkable=false,
-tiles={"default_stone.png"},
-drop = 'ghostblocks:stone',
-diggable=true,
-groups = {cracky=3, stone=1},
-})
-minetest.register_node("ghostblocks:stone_with_iron", {
-walkable=false,
-tiles={"default_stone.png^default_mineral_iron.png"},
-drop = 'ghostblocks:stone_with_iron',
-diggable=true,
-groups = {cracky=3, stone=1},
-})
-minetest.register_node("ghostblocks:stone_with_coal", {
-walkable=false,
-tiles={"default_stone.png^default_mineral_coal.png"},
-drop = 'ghostblocks:stone_with_coal',
-diggable=true,
-groups = {cracky=3, stone=1},
-})
-minetest.register_node("ghostblocks:stone_with_diamond", {
-walkable=false,
-tiles={"default_stone.png^default_mineral_diamond.png"},
-drop = 'ghostblocks:stone_with_diamond',
-diggable=true,
-groups = {cracky=3, stone=1},
-})
-minetest.register_node("ghostblocks:stone_with_mese", {
-walkable=false,
-tiles={"default_stone.png^default_mineral_mese.png"},
-drop = 'ghostblocks:stone_with_mese',
-diggable=true,
-groups = {cracky=3, stone=1},
-})
-minetest.register_node("ghostblocks:stone_with_copper", {
-walkable=false,
-tiles={"default_stone.png^default_mineral_copper.png"},
-drop = 'ghostblocks:stone_with_copper',
-diggable=true,
-groups = {cracky=3, stone=1},
-})
-minetest.register_node("ghostblocks:stone_with_gold", {
-walkable=false,
-tiles={"default_stone.png^default_mineral_gold.png"},
-drop = 'ghostblocks:stone_with_gold',
-diggable=true,
-groups = {cracky=3, stone=1},
-})
-minetest.register_node("ghostblocks:goldblock", {
-walkable=false,
-tiles={"default_gold_block.png"},
-drop = 'ghostblocks:goldblock',
-diggable=true,
-groups = {cracky=3, stone=1},
-})
-minetest.register_node("ghostblocks:coalblock", {
-walkable=false,
-tiles={"default_coal_block.png"},
-drop = 'ghostblocks:coalblock',
-diggable=true,
-groups = {cracky=3, stone=1},
-})
-minetest.register_node("ghostblocks:steelblock", {
-walkable=false,
-tiles={"default_steel_block.png"},
-drop = 'ghostblocks:steelblock',
-diggable=true,
-groups = {cracky=3, stone=1},
-})
-minetest.register_node("ghostblocks:diamondblock", {
-walkable=false,
-tiles={"default_diamond_block.png"},
-drop = 'ghostblocks:diamondblock',
-diggable=true,
-groups = {cracky=3, stone=1},
-})
-minetest.register_node("ghostblocks:mese", {
-walkable=false,
-tiles={"default_mese_block.png"},
-drop = 'ghostblocks:mese',
-diggable=true,
-groups = {cracky=3, stone=1},
-})
-minetest.register_node("ghostblocks:bronzeblock", {
-walkable=false,
-tiles={"default_bronze_block.png"},
-drop = 'ghostblocks:bronzeblock',
-diggable=true,
-groups = {cracky=3, stone=1},
-})
-minetest.register_node("ghostblocks:copperblock", {
-walkable=false,
-tiles={"default_copper_block.png"},
-drop = 'ghostblocks:copperblock',
-diggable=true,
-groups = {cracky=3, stone=1},
-})
-minetest.register_node("ghostblocks:mossycobble", {
-walkable=false,
-tiles={"default_mossycobble.png"},
-drop = 'ghostblocks:mossycobble',
-diggable=true,
-groups = {cracky=3, stone=1},
-})
-minetest.register_node("ghostblocks:cobble", {
-walkable=false,
-tiles={"default_cobble.png"},
-drop = 'ghostblocks:cobble',
-diggable=true,
-groups = {cracky=3, stone=1},
-})
-minetest.register_node("ghostblocks:stonebrick", {
-walkable=false,
-tiles={"default_stone_brick.png"},
-drop = 'ghostblocks:stonebrick',
-diggable=true,
-groups = {cracky=3, stone=1},
-})
-minetest.register_node("ghostblocks:desert_stone", {
-walkable=false,
-tiles={"default_desert_stone.png"},
-drop = 'ghostblocks:desert_stone',
-diggable=true,
-groups = {cracky=3, stone=1},
-})
-minetest.register_node("ghostblocks:desert_stonebrick", {
-walkable=false,
-tiles={"default_desert_stone_brick.png"},
-drop = 'ghostblocks:desert_stonebrick',
-diggable=true,
-groups = {cracky=3, stone=1},
-})
-minetest.register_node("ghostblocks:desert_cobble", {
-walkable=false,
-tiles={"default_desert_cobble.png"},
-drop = 'ghostblocks:desert_cobble',
-diggable=true,
-groups = {cracky=3, stone=1},
-})
-minetest.register_node("ghostblocks:sandstone", {
-walkable=false,
-tiles={"default_sandstone.png"},
-drop = 'ghostblocks:sandstone',
-diggable=true,
-groups = {cracky=3, stone=1},
-})
-minetest.register_node("ghostblocks:sandstonebrick", {
-walkable=false,
-tiles={"default_sandstone_brick.png"},
-drop = 'ghostblocks:sandstonebrick',
-diggable=true,
-groups = {cracky=3, stone=1},
-})
-minetest.register_node("ghostblocks:obsidian", {
-walkable=false,
-tiles={"default_obsidian.png"},
-drop = 'ghostblocks:obsidian',
-diggable=true,
-groups = {cracky=3, stone=1},
-})
-minetest.register_node("ghostblocks:obsidianbrick", {
-walkable=false,
-tiles={"default_obsidian_brick.png"},
-drop = 'ghostblocks:obsidianbrick',
-diggable=true,
-groups = {cracky=3, stone=1},
-})
-minetest.register_node("ghostblocks:snowblock", {
-walkable=false,
-tiles={"default_snow_block.png"},
-drop = 'ghostblocks:snowblock',
-diggable=true,
-groups = {cracky=3, stone=1},
-})
-minetest.register_node("ghostblocks:gravel", {
-walkable=false,
-tiles={"default_gravel.png"},
-drop = 'ghostblocks:gravel',
-diggable=true,
-groups = {cracky=3, stone=1},
-})
-minetest.register_node("ghostblocks:desert_sand", {
-walkable=false,
-tiles={"default_desert_sand.png"},
-drop = 'ghostblocks:desert_sand',
-diggable=true,
-groups = {cracky=3, stone=1},
-})
+end
 --[[
 minetest.register_node("ghostblocks:ghost_converter"{
 walkable=true,
